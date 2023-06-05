a9a9c79cbec974d752de79a8f0654b90d4e7e395
Darrell-Rafa <drafa0609@gmail.com>
2023-06-05 20:20:47 +0700

    fix z-index

--- app/components/SocialsFixed.tsx
+++ app/components/SocialsFixed.tsx
@@ -13,7 +13,7 @@ const firaCode = Fira_Code({
 function SocialsFixed() {
   return (
     <section id="socials-fixed">
-      <div className="hidden lg:flex flex-col gap-6 fixed left-12 bottom-0">
+      <div className="hidden lg:flex flex-col gap-6 fixed left-12 bottom-0 z-10">
         <RevealWrapper origin="bottom">
           <a href="https://github.com/darrellrahan">
             <FiGithub
@@ -34,7 +34,7 @@ function SocialsFixed() {
           <div className="w-[1.5px] mx-auto h-[5.5rem] bg-[#a8b2d1]"></div>
         </RevealWrapper>
       </div>
-      <div className="hidden lg:flex flex-col fixed -right-8 bottom-0">
+      <div className="hidden lg:flex flex-col fixed -right-8 bottom-0 z-10">
         <a
           className={`${firaCode.className} text-sm tracking-[1.5px] -translate-y-28 rotate-90 text-[#a8b2d1] hover:text-[#64ffda] hover:-translate-y-[7.1875rem] duration-100 ease-linear`}
           href="mailto:drafa0609@gmail.com"
